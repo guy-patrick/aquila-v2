42bb6f909b7e12ea7738e13dd3cadbb05056117d
Patrick OUAMBO <patgp04@gmail.com>
2022-08-15 17:42:15 +0100

    added functions, header and footer php files

--- index.php
+++ index.php
@@ -4,18 +4,11 @@
  *
  * @package Aquila
 */
+    get_header();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wordpress theme</title>
-</head>
-<body>
-    <h1>hello wordpress</h1>
-</body>
-</html>
+    <div class="content">This is the content !</div>
+<?php
+    get_footer();
 
 
 
