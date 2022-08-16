cda851609cbb5c2845e3caa6d047ac175f1925aa
Patrick OUAMBO <patgp04@gmail.com>
2022-08-16 18:21:17 +0100

    added assets, screenshot and stylesheet / scripts enqueues in functions.php file

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body <?php body_class("") ?>>
+<body>
     <?php
         if ( function_exists( 'wp_body_open' ) ) {
             wp_body_open();
