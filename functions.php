61f3943a1bff9d5504381d2b458bec340add2d3a
Patrick OUAMBO <patgp04@gmail.com>
2022-08-16 18:45:21 +0100

    enqueue boostrap style and scripts

--- functions.php
+++ functions.php
@@ -10,11 +10,21 @@
 // wp_die();
 
 function aquila_enqueue_scripts () {
+    // register styles
     wp_register_style( 'style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all' );
-    wp_register_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true );
+    wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/assets/src/library/bootstrap/css/bootstrap.min.css', [], false, 'all' );
 
+    // register scripts
+    wp_register_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true );
+    wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/assets/src/library/bootstrap/js/bootstrap.min.js', ['jquery'], false, true );
+
+    // enqueue styles
     wp_enqueue_style( 'style-css' );
+    wp_enqueue_style( 'bootstrap-css' );
+
+    // enqueue styles
     wp_enqueue_script( 'main-js' );
+    wp_enqueue_script( 'bootstrap-js' );
 }
 
 add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
